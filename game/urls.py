3a623d19934e6584aaf9b1dc793ff618489c00aa
Michal Mazurek <akfaew@gmail.com>
2014-07-08 15:01:21 +0000

    forgot one bit in nations/private graphs cleanup

--- game/urls.py
+++ game/urls.py
@@ -6,5 +6,4 @@ from longturn.views import *
 urlpatterns = patterns('',
 	url(r'^$',				game_list, name='game_list'),
 	url(r'^([a-zA-Z0-9]+)/$',		game, name='game'),
-	url(r'^([a-zA-Z0-9]+)/([a-zA-Z0-9_-]+)/$',	nation, name='nation'),
 )
